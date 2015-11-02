f1b1bc20e0aea42fab577ff3e1bc0debd79bb049
laazer <brandt.j@husky.neu.edu>
2015-11-01 20:39:44 -0500

    updated

--- Portal-Plus-Plus/Portal-Plus-Plus/gravity_obj.h
+++ Portal-Plus-Plus/Portal-Plus-Plus/gravity_obj.h
@@ -1,12 +1,14 @@
 #pragma once
 #include "velocity.h"
 #include "posn.h"
+#include "game_object.h"
 #define GRAV_ACCEL -1.0
 // acceleration due to gravity is in pixels per game tick
 
 
-class GravityObject {
+class GravityObject : public GameObject{
 public:
+	GravityObject()
 	Velocity applyGravity();
 	Velocity getVelocity();
 	GravityObject * updateVel(double dx, double dy);
