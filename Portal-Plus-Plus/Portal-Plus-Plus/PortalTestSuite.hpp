0f02e80bacf5974ed34d6d4107fb5e34102dfab1
austin <austin@akilduff.com>
2015-11-29 16:19:16 -0500

    Started test file

---
+++ Portal-Plus-Plus/Portal-Plus-Plus/PortalTestSuite.hpp
@@ -0,0 +1,18 @@
+#include <cxxtest/TestSuite.h>
+#include <iostream>
+#include "main.cpp"
+
+class PortalTestSuite : public CxxTest::TestSuite {
+public:
+	// Tests for Enemy
+	// Tests for Game Model
+	// Tests for Game Object
+	// Tests for Key Handler
+	// Tests for Level View
+	// Tests for Mouse Handler
+	// Tests for Player
+	// Tests for Portal
+	// Tests for Posn
+	// Tests for Velocity
+	// Tests for World
+};
