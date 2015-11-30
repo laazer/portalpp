e2f66e6c02742fa02236fe9a2b371c68c2c6ba55
austin <austin@akilduff.com>
2015-11-30 18:27:24 -0500

    Outlined tests for every method so far

--- Portal-Plus-Plus/Portal-Plus-Plus/PortalTestSuite.hpp
+++ Portal-Plus-Plus/Portal-Plus-Plus/PortalTestSuite.hpp
@@ -5,14 +5,181 @@
 class PortalTestSuite : public CxxTest::TestSuite {
 public:
 	// Tests for Enemy
+	void enemySet() {
+
+	}
+	void enemyUpdate() {
+
+	}
+	void enemyCollision() {
+
+	}
+
 	// Tests for Game Model
+	void gameModelConstructor1() {
+
+	}
+	void gameModelConstructor2() {
+
+	}
+	void gameModelDestructor() {
+
+	}
+	void gameModelUpdate() {
+
+	}
+	void gameModelLaunchPortal() {
+
+	}
+	void gameModelCheckPortals() {
+
+	}
+	void gameModelGetRenderObjects() {
+
+	}
+	void gameModelGetPlayer() {
+
+	}
+
 	// Tests for Game Object
+	void gameObjectMoveX() {
+
+	}
+	void gameObjectMoveY() {
+
+	}
+
 	// Tests for Key Handler
+	void keyHandlerConstructor1() {
+
+	}
+	void keyHandlerConstructor2() {
+
+	}
+	void keyHandlerHandleKey() {
+
+	}
+
 	// Tests for Level View
+	void levelViewConstructor1() {
+
+	}
+	void levelViewConstructor2() {
+
+	}
+	void levelViewRender() {
+
+	}
+
+	// No tests for Main
+
 	// Tests for Mouse Handler
+	void mouseHandlerConstructor1() {
+
+	}
+	void mouseHandlerConstructor2() {
+
+	}
+	void mouseHandlerHandleMouse() {
+
+	}
+
 	// Tests for Player
+	void playerConstructor() {
+
+	}
+	void playerUpdate() {
+
+	}
+	void playerCollision() {
+
+	}
+	void playerMoveLeft() {
+
+	}
+	void playerJump() {
+
+	}
+	void playerMoveRight() {
+
+	}
+
 	// Tests for Portal
+	void portalConstructor() {
+
+	}
+	void portalUpdate() {
+
+	}
+	void portalCollision() {
+
+	}
+	void portalTeleport() {
+
+	}
+
 	// Tests for Posn
+	void posnConstructor1() {
+
+	}
+	void posnConstructor2() {
+
+	}
+	void posnGetX() {
+
+	}
+	void posnGetY() {
+
+	}
+	void posnSetX() {
+
+	}
+	void posnSetY() {
+
+	}
+	void posnSet() {
+
+	}
+	void posnMove1() {
+
+	}
+	void posnMove2() {
+
+	}
+	void posnIsWithin() {
+
+	}
+	void posnEquals() {
+
+	}
+	void posnNotEquals() {
+
+	}
+
 	// Tests for Velocity
-	// Tests for World
+	void velocityConstructor1() {
+
+	}
+	void velocityConstructor2() {
+
+	}
+	void velocityGetMagnitude() {
+
+	}
+	void velocityGetX() {
+
+	}
+	void velocityGetY() {
+
+	}
+	void velocitySetX() {
+
+	}
+	void velocitySetY() {
+
+	}
+	void velocityAssignment() {
+
+	}
+	// No tests for World
 };
